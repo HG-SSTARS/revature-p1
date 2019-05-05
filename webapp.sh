2f79eb332d9f8ae3e3ba6c0324acef7b093ee201
tasnim20 <36062093+HG-SSTARS@users.noreply.github.com>
2019-05-04 23:08:30 -0400

    Update webapp.sh

--- webapp.sh
+++ webapp.sh
@@ -10,6 +10,13 @@ az group create --name $group --location southcentralus
 ## Create an App Service plan in FREE tier.
 az appservice plan create --name $serviceplan --resource-group $group --number-of-workers 3 --sku B1 --is-linux
 
+## Create an storage account with storage kind
+az storage account create --name webappstorage --sku Standard_LRS --resource-group $group \
+--kind blobStorage --location southcentralus --access-tier Hot 
+
+## Create an container under existing storage account and kind of storage
+az storage container create --name blobcontainer --public-access blob --account-key
+
 ## Create a web app.
 az webapp create --name $webappname --resource-group $group --plan $serviceplan -r "node|10.14"
 
