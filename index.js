968a596a0d4d39f4a42e70c826f59bf98fbcceb4
tasnim20 <36062093+HG-SSTARS@users.noreply.github.com>
2019-05-05 20:25:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-let learn = document.querySelector("#sections")
+let learn = document.querySelector("#section")
 
 if(learn){
 
    fetch('/images')
    .then(res => res.json())
    .then(res => {
+        console.log(res)
        res.forEach(image =>{
          const img = document.createElement('img')
-         img.src = `/uploads/${image}`
+         img.src = img.src = `https://${process.env.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net/images/${image}`
 
          learn.appendChild(img)  //Node.appendChild() method adds a node to the end of the list of children of a specified parent node.
        })
    });
+
+
+
 }
